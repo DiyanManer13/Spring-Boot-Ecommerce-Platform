3eab9331f4ce77e6b3d264cd2bbdb829a9c80db7
UnknownAxon13 <diyanmaner06@gmail.com>
2026-07-06 04:56:17 +0530

    GeneratedValue of type IDENTITY for auto incrementing id

--- src/main/java/com/ecommerce/project/model/Category.java
+++ src/main/java/com/ecommerce/project/model/Category.java
@@ -1,12 +1,15 @@
 package com.ecommerce.project.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import org.jspecify.annotations.NonNull;
 
 @Entity(name = "CATEGORIES")
-public class Category {
+public class Category{
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // there are different generation types like auto,identity,sequence,table
     private Long categoryId;
     private String categoryName;
 
