37c7e80c9b43149b7bdc5b4a1fb08f541454f0a9
UnknownAxon13 <diyanmaner06@gmail.com>
2026-07-17 21:02:55 +0530

    Valid annotation for not getting 500 error when NotBlank hits

--- src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -2,6 +2,7 @@ package com.ecommerce.project.controller;
 
 import com.ecommerce.project.model.Category;
 import com.ecommerce.project.service.CategoryService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
@@ -26,7 +27,7 @@ public class CategoryController {
 
     // @PostMapping("/api/public/categories")
     @RequestMapping(value="/public/categories",method = RequestMethod.POST)
-    public ResponseEntity<String> createCategory(@RequestBody Category category){
+    public ResponseEntity<String> createCategory(@Valid @RequestBody Category category){
 
             categoryService.createCategory(category);
             return new ResponseEntity<>("Category Added Successfully",HttpStatus.CREATED);
